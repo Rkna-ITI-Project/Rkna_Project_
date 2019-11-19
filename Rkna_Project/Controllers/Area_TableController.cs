20f474c35d2d54563aa10f923b0a1ad6e3ccb0eb
ahmed dorar <ahmeddorar99@hotmail.com>
2019-11-19 18:12:05 +0200

    Project Without API

---
+++ Rkna_Project/Controllers/Area_TableController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rkna_Project.Models;
+
+namespace Rkna_Project.Controllers
+{
+    public class Area_TableController : Controller
+    {
+        private Rkna_DataBaseEntities db = new Rkna_DataBaseEntities();
+
+        // GET: Area_Table
+        public ActionResult Index()
+        {
+            var area_Table = db.Area_Table.Include(a => a.AspNetUser).Include(a => a.AspNetUser1).Include(a => a.States_Table);
+            return View(area_Table.ToList());
+        }
+
+        // GET: Area_Table/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area_Table area_Table = db.Area_Table.Find(id);
+            if (area_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(area_Table);
+        }
+
+        // GET: Area_Table/Create
+        public ActionResult Create()
+        {
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.States_ID = new SelectList(db.States_Table, "States_ID", "States_Name");
+            return View();
+        }
+
+        // POST: Area_Table/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Area_ID,States_ID,Area_Name,Area_Desc,Area_X_Point,Area_Y_Point,Area_Manger,Area_Hour_Rate,Area_Start_Time,Area_End_Time")] Area_Table area_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Area_Table.Add(area_Table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.States_ID = new SelectList(db.States_Table, "States_ID", "States_Name", area_Table.States_ID);
+            return View(area_Table);
+        }
+
+        // GET: Area_Table/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area_Table area_Table = db.Area_Table.Find(id);
+            if (area_Table == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.States_ID = new SelectList(db.States_Table, "States_ID", "States_Name", area_Table.States_ID);
+            return View(area_Table);
+        }
+
+        // POST: Area_Table/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Area_ID,States_ID,Area_Name,Area_Desc,Area_X_Point,Area_Y_Point,Area_Manger,Area_Hour_Rate,Area_Start_Time,Area_End_Time")] Area_Table area_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(area_Table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.Area_Manger = new SelectList(db.AspNetUsers, "Id", "Email", area_Table.Area_Manger);
+            ViewBag.States_ID = new SelectList(db.States_Table, "States_ID", "States_Name", area_Table.States_ID);
+            return View(area_Table);
+        }
+
+        // GET: Area_Table/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Area_Table area_Table = db.Area_Table.Find(id);
+            if (area_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(area_Table);
+        }
+
+        // POST: Area_Table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Area_Table area_Table = db.Area_Table.Find(id);
+            db.Area_Table.Remove(area_Table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Rkna_Project/Controllers/Customer_Slut_TableController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rkna_Project.Models;
+
+namespace Rkna_Project.Controllers
+{
+    public class Customer_Slut_TableController : Controller
+    {
+        private Rkna_DataBaseEntities db = new Rkna_DataBaseEntities();
+
+        // GET: Customer_Slut_Table
+        public ActionResult Index()
+        {
+            var customer_Slut_Table = db.Customer_Slut_Table.Include(c => c.AspNetUser).Include(c => c.Car_Specifications_Table).Include(c => c.Slut_Table);
+            return View(customer_Slut_Table.ToList());
+        }
+
+        // GET: Customer_Slut_Table/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer_Slut_Table customer_Slut_Table = db.Customer_Slut_Table.Find(id);
+            if (customer_Slut_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_Slut_Table);
+        }
+
+        // GET: Customer_Slut_Table/Create
+        public ActionResult Create()
+        {
+            ViewBag.Customer_ID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.Car_Spe_ID = new SelectList(db.Car_Specifications_Table, "Car_Spe_ID", "Car_Owner_ID");
+            ViewBag.Slut_ID = new SelectList(db.Slut_Table, "Slut_ID", "Name");
+            return View();
+        }
+
+        // POST: Customer_Slut_Table/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Customer_Slut_ID,Customer_ID,Slut_ID,Car_Spe_ID,Cus_Slut_Date,Cus_Slut_S_Time,Cus_Slut_E_Time,Cheeck_Code")] Customer_Slut_Table customer_Slut_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Customer_Slut_Table.Add(customer_Slut_Table);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Customer_ID = new SelectList(db.AspNetUsers, "Id", "Email", customer_Slut_Table.Customer_ID);
+            ViewBag.Car_Spe_ID = new SelectList(db.Car_Specifications_Table, "Car_Spe_ID", "Car_Owner_ID", customer_Slut_Table.Car_Spe_ID);
+            ViewBag.Slut_ID = new SelectList(db.Slut_Table, "Slut_ID", "Name", customer_Slut_Table.Slut_ID);
+            return View(customer_Slut_Table);
+        }
+
+        // GET: Customer_Slut_Table/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer_Slut_Table customer_Slut_Table = db.Customer_Slut_Table.Find(id);
+            if (customer_Slut_Table == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Customer_ID = new SelectList(db.AspNetUsers, "Id", "Email", customer_Slut_Table.Customer_ID);
+            ViewBag.Car_Spe_ID = new SelectList(db.Car_Specifications_Table, "Car_Spe_ID", "Car_Owner_ID", customer_Slut_Table.Car_Spe_ID);
+            ViewBag.Slut_ID = new SelectList(db.Slut_Table, "Slut_ID", "Name", customer_Slut_Table.Slut_ID);
+            return View(customer_Slut_Table);
+        }
+
+        // POST: Customer_Slut_Table/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Customer_Slut_ID,Customer_ID,Slut_ID,Car_Spe_ID,Cus_Slut_Date,Cus_Slut_S_Time,Cus_Slut_E_Time,Cheeck_Code")] Customer_Slut_Table customer_Slut_Table)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(customer_Slut_Table).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Customer_ID = new SelectList(db.AspNetUsers, "Id", "Email", customer_Slut_Table.Customer_ID);
+            ViewBag.Car_Spe_ID = new SelectList(db.Car_Specifications_Table, "Car_Spe_ID", "Car_Owner_ID", customer_Slut_Table.Car_Spe_ID);
+            ViewBag.Slut_ID = new SelectList(db.Slut_Table, "Slut_ID", "Name", customer_Slut_Table.Slut_ID);
+            return View(customer_Slut_Table);
+        }
+
+        // GET: Customer_Slut_Table/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Customer_Slut_Table customer_Slut_Table = db.Customer_Slut_Table.Find(id);
+            if (customer_Slut_Table == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer_Slut_Table);
+        }
+
+        // POST: Customer_Slut_Table/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Customer_Slut_Table customer_Slut_Table = db.Customer_Slut_Table.Find(id);
+            db.Customer_Slut_Table.Remove(customer_Slut_Table);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
