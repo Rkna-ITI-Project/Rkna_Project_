b46bc390ea5c54b805fe6dac94b2149f5df0ad7f
ahmed dorar <ahmeddorar99@hotmail.com>
2019-11-22 17:40:22 +0200

    update google map

--- Rkna_Project/App_Start/BundleConfig.cs
+++ Rkna_Project/App_Start/BundleConfig.cs
@@ -14,7 +14,9 @@ namespace Rkna_Project
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                       "~/Scripts/Site.js"));
+
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
@@ -25,6 +27,7 @@ namespace Rkna_Project
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
